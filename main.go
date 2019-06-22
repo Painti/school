2aeb597b986e7156ce25568978bce5cb81bc217e
Teerapat Voradanunt <teerapat.v@kbtg.tech>
2019-06-22 17:27:22 +0700

    Finish

--- main.go
+++ main.go
@@ -2,56 +2,173 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
 
 type Todo struct {
-	ID     int
-	Title  string
-	Status string
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Status string `json:"status"`
 }
 
-func getTodos(c *gin.Context) {
-	url := "postgres://xqxxogxt:DtBnIRdBp82_ts8pjBBdL2WUIWJx5mXc@satao.db.elephantsql.com:5432/xqxxogxt"
-	db, err := sql.Open("postgres", url)
+func getTodosFromDB(db *sql.DB) (todos []Todo, err error) {
+	query := `SELECT id, title, status FROM todos`
+	stmt, pErr := db.Prepare(query)
+	if pErr != nil {
+		err = pErr
+		return
+	}
+	rows, qErr := stmt.Query()
+	if qErr != nil {
+		err = qErr
+		return
+	}
+	for rows.Next() {
+		t := Todo{}
+		sErr := rows.Scan(&t.ID, &t.Title, &t.Status)
+		if sErr != nil {
+			err = sErr
+			return
+		}
+		todos = append(todos, t)
+	}
+	return
+}
+
+func getTodoByIDFromDB(db *sql.DB, id int) (t Todo, err error) {
+	query := `SELECT id, title, status FROM todos WHERE id=$1`
+	stmt, pErr := db.Prepare(query)
+	if pErr != nil {
+		err = pErr
+		return
+	}
+	row := stmt.QueryRow(id)
+	sErr := row.Scan(&t.ID, &t.Title, &t.Status)
+	if sErr != nil {
+		err = sErr
+		return
+	}
+	return
+}
+
+func createTodo(db *sql.DB, todo Todo) (id int, err error) {
+	query := `INSERT INTO todos(title, status) VALUES($1, $2) RETURNING id`
+	row := db.QueryRow(query, todo.Title, todo.Status)
+	sErr := row.Scan(&id)
+	if sErr != nil {
+		err = sErr
+	}
+	return
+}
+
+func deleteTodo(db *sql.DB, id int) (err error) {
+	query := `DELETE FROM todos WHERE id=$1`
+	stmt, pErr := db.Prepare(query)
+	if pErr != nil {
+		err = pErr
+		return
+	}
+	result, eErr := stmt.Exec(id)
+	if eErr != nil {
+		err = eErr
+	}
+
+	row, rErr := result.RowsAffected()
+	if rErr != nil {
+		err = rErr
+	}
+	if row == 0 {
+		err = errors.New(fmt.Sprintf("The todo ID:%d is not exists.", id))
+	}
+	return
+}
+
+func getTodosHandler(c *gin.Context, db *sql.DB) {
+	todos, err := getTodosFromDB(db)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
-	defer db.Close()
-	fmt.Println("DB Connected")
+	c.JSON(http.StatusOK, todos)
+}
 
-	query := `SELECT id, title, status FROM todos`
-	stmt, err := db.Prepare(query)
-
-	rows, _ := stmt.Query()
-
-	tt := []Todo{}
-
-	for rows.Next() {
-		t := Todo{}
-		err = rows.Scan(&t.ID, &t.Title, &t.Status)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-		tt = append(tt, t)
+func getTodoByIDHandler(c *gin.Context, db *sql.DB) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
 
-	c.JSON(http.StatusOK, tt)
+	todo, err := getTodoByIDFromDB(db, id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, todo)
+}
+
+func createTodoHandler(c *gin.Context, db *sql.DB) {
+	var t Todo
+	err := c.BindJSON(&t)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	id, err := createTodo(db, t)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	t.ID = id
+	c.JSON(http.StatusOK, t)
+}
+
+func deleteTodoHandler(c *gin.Context, db *sql.DB) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = deleteTodo(db, id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"status": "success"})
 }
 
 func main() {
+	url := os.Getenv("DATABASE_URL")
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		fmt.Println("DB connect failed")
+		return
+	}
+	defer db.Close()
+
 	r := gin.Default()
-
-	// r.GET("/students", getStudentHandler)
-	// r.POST("/students", postStudentHandler)
-
-	r.GET("/api/todos", getTodos)
+	r.GET("/api/todos", func(c *gin.Context) {
+		getTodosHandler(c, db)
+	})
+	r.GET("/api/todos/:id", func(c *gin.Context) {
+		getTodoByIDHandler(c, db)
+	})
+	r.POST("/api/todos", func(c *gin.Context) {
+		createTodoHandler(c, db)
+	})
+	r.DELETE("/api/todos/:id", func(c *gin.Context) {
+		deleteTodoHandler(c, db)
+	})
 
 	r.Run(":1234")
+	fmt.Println("Server started")
+
 }
